1e0c8d2ea54b254cbcc0fbefd4ea8bbd02b9c787
Jisung <jisungkim215@gmail.com>
2022-04-17 04:33:33 -0400

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
+  // { path: 'add', component: AddpageComponent },
+  // { path: 'show', component: ShowpageComponent },
+  // { path: 'settings', component: SettingspageComponent },
+  // { path: 'modify/:id', component: ModifypageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'jisung',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Angular';
+  title = 'Angular Mobile App';
 }
